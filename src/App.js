2b9ed8ad99209def0e69902f1d09928ff2c010c6
Sahidul Islam <jonyahmed19@gmail.com>
2022-02-23 19:43:10 +0600

    Code Splitting for performance

--- src/App.js
+++ src/App.js
@@ -1,25 +1,33 @@
+import React, { Suspense } from "react";
 import { Route, Routes } from "react-router-dom";
 import "./App.css";
-import Cartpage from "./pages/Cartpage/Cartpage";
-import Contactpage from "./pages/Contact/Contact.pages";
-import Home from "./pages/Homepage/Home.pages";
-import Shoppage from "./pages/Shop/Shop.page";
-import SearchResult from "./pages/SearchResult/SearchResult.pages";
-import Notfound from "./pages/Notfound/Notfound.pages";
-import SingleProduct from "./pages/SingleProduct/SingleProduct.pages";
+
+const Home = React.lazy(() => import("./pages/Homepage/Home.pages"));
+const Cartpage = React.lazy(() => import("./pages/Cartpage/Cartpage"));
+const Contactpage = React.lazy(() => import("./pages/Contact/Contact.pages"));
+const Shoppage = React.lazy(() => import("./pages/Shop/Shop.page"));
+const SearchResult = React.lazy(() =>
+  import("./pages/SearchResult/SearchResult.pages")
+);
+const Notfound = React.lazy(() => import("./pages/Notfound/Notfound.pages"));
+const SingleProduct = React.lazy(() =>
+  import("./pages/SingleProduct/SingleProduct.pages")
+);
 
 function App() {
   return (
     <div className="App">
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/cart" element={<Cartpage />} />
-        <Route path="/contact" element={<Contactpage />} />
-        <Route path="/shop" element={<Shoppage />} />
-        <Route path="/search" element={<SearchResult />} />
-        <Route path="/product/:productId" element={<SingleProduct />} />
-        <Route path="*" element={<Notfound />} />
-      </Routes>
+      <Suspense fallback={<div className="loader">Loading</div>}>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/cart" element={<Cartpage />} />
+          <Route path="/contact" element={<Contactpage />} />
+          <Route path="/shop" element={<Shoppage />} />
+          <Route path="/search" element={<SearchResult />} />
+          <Route path="/product/:productId" element={<SingleProduct />} />
+          <Route path="*" element={<Notfound />} />
+        </Routes>
+      </Suspense>
     </div>
   );
 }
